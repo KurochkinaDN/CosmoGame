f0d651cdf613e69d11d238fd5b9366a599e86ed6
KurochkinaDN <132352044+KurochkinaDN@users.noreply.github.com>
2023-05-02 16:27:36 +0500

    Add files via upload

---
+++ CosmoGame.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Cosmo_Game
+{
+    partial class CosmoGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtKillers = new System.Windows.Forms.Label();
+            this.txtHealth = new System.Windows.Forms.Label();
+            this.HealthBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAmmo.ForeColor = System.Drawing.Color.Bisque;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 12);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(107, 29);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtKillers
+            // 
+            this.txtKillers.AutoSize = true;
+            this.txtKillers.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtKillers.ForeColor = System.Drawing.Color.Bisque;
+            this.txtKillers.Location = new System.Drawing.Point(396, 12);
+            this.txtKillers.Name = "txtKillers";
+            this.txtKillers.Size = new System.Drawing.Size(106, 29);
+            this.txtKillers.TabIndex = 1;
+            this.txtKillers.Text = "Killers: 0";
+            this.txtKillers.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.AutoSize = true;
+            this.txtHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtHealth.ForeColor = System.Drawing.Color.Bisque;
+            this.txtHealth.Location = new System.Drawing.Point(604, 12);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(88, 29);
+            this.txtHealth.TabIndex = 2;
+            this.txtHealth.Text = "Health:";
+            // 
+            // HealthBar
+            // 
+            this.HealthBar.Location = new System.Drawing.Point(698, 12);
+            this.HealthBar.Name = "HealthBar";
+            this.HealthBar.Size = new System.Drawing.Size(212, 29);
+            this.HealthBar.TabIndex = 3;
+            this.HealthBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::Cosmo_Game.Properties.Resources.roket;
+            this.player.Location = new System.Drawing.Point(302, 325);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(217, 279);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // CosmoGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(922, 653);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.HealthBar);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.txtKillers);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "CosmoGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtKillers;
+        private System.Windows.Forms.Label txtHealth;
+        private System.Windows.Forms.ProgressBar HealthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ CosmoGame.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cosmo_Game
+{
+    public partial class CosmoGame : Form
+    {
+        bool goLeft, goRight, goDown, goUp, gameOver;
+        string facing = "up";
+        int rocketHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int ufoSpeed = 3;
+        Random randNum = new Random();
+        List<PictureBox> ufoList=new List<PictureBox>();
+        int score;
+
+        public CosmoGame()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (rocketHealth > 1)
+            {
+                HealthBar.Value = rocketHealth;
+            }
+            else
+            {
+                gameOver = true;
+                player.Image = Properties.Resources.dead;
+                GameTimer.Stop();
+            }
+
+            txtAmmo.Text = "Ammo: " + ammo;
+            txtKillers.Text = "Kills: " + score;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+            if (goUp == true && player.Top > 45)
+            {
+                player.Top -= speed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        ammo += 5;
+
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "ufo")
+                {
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        rocketHealth -= 1;
+                    }
+
+                    if (x.Left > player.Left)
+                    {
+                        x.Left -= ufoSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.uleft;
+                    }
+                    if (x.Left < player.Left)
+                    {
+                        x.Left += ufoSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.uright;
+                    }
+                    if (x.Top > player.Top)
+                    {
+                        x.Top -= ufoSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.uup;
+                    }
+                    if (x.Top < player.Top)
+                    {
+                        x.Top += ufoSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.udown;
+                    }
+
+                }
+
+                foreach (Control j in this.Controls)
+                {
+                    if (j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "ufo")
+                    {
+                        if (x.Bounds.IntersectsWith(j.Bounds))
+                        {
+                            score++;
+
+                            this.Controls.Remove(j);
+                            ((PictureBox)j).Dispose();
+                            this.Controls.Remove(x);
+                            ((PictureBox)x).Dispose();
+                            ufoList.Remove(((PictureBox)x));
+                            MakeUfo();
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (gameOver == true)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space && ammo > 0 && gameOver == false)
+            {
+                ammo--;
+                ShootBullet(facing);
+
+
+                if (ammo < 1)
+                {
+                    DropAmmo();
+                }
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void ShootBullet(string direction)
+        {
+            Bullet shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+        }
+        private void DropAmmo()
+        {
+
+            PictureBox ammo = new PictureBox();
+            ammo.Image = Properties.Resources.ammo_Image;
+            ammo.SizeMode = PictureBoxSizeMode.AutoSize;
+            ammo.Left = randNum.Next(10, this.ClientSize.Width - ammo.Width);
+            ammo.Top = randNum.Next(60, this.ClientSize.Height - ammo.Height);
+            ammo.Tag = "ammo";
+            this.Controls.Add(ammo);
+
+            ammo.BringToFront();
+            player.BringToFront();
+
+
+
+        }
+        private void MakeUfo()
+        {
+            PictureBox ufo = new PictureBox();
+            ufo.Tag = "ufo";
+            ufo.Image = Properties.Resources.UFO;
+            ufo.Left = randNum.Next(0, 900);
+            ufo.Top = randNum.Next(0, 800);
+            ufo.SizeMode = PictureBoxSizeMode.AutoSize;
+            ufoList.Add(ufo);
+            this.Controls.Add(ufo);
+            player.BringToFront();
+        }
+
+        private void RestartGame()
+        {
+            PictureBox ammo = new PictureBox();
+            ammo.Image = Properties.Resources.UFO;
+            ammo.SizeMode = PictureBoxSizeMode.AutoSize;
+            ammo.Left = randNum.Next(10, this.ClientSize.Width - ammo.Width);
+            ammo.Top = randNum.Next(60, this.ClientSize.Height - ammo.Height);
+            ammo.Tag = "ammo";
+            this.Controls.Add(ammo);
+
+            ammo.BringToFront();
+            player.BringToFront();
+        }
+    }
+}
